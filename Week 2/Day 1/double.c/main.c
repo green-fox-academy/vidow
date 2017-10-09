95aaa42ce56c42c76b8e819abf3179003e8b30d3
Vidow <szucsnorbert@outlook.com>
2017-10-09 21:40:48 +0200

    Added double.c exercise

---
+++ Week 2/Day 1/double.c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "double.h"
+
+// create a function called `doubling` that doubles it's input parameter
+// again the parameters value should be stored in a .h file
+
+
+int main()
+{
+    dd(a);
+    dd(4);
+    dd(110);
+    dd(27);
+
+    return 0;
+}
+
+void dd(int NUMBER)
+{
+    printf("If we double %d the result is", NUMBER);
+
+    printf(" %d.\n", NUMBER * 2);
+
+    return;
+}
