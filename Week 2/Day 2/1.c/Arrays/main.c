395328b7ba2d15398457b0068ae819915dd34168
Vidow <szucsnorbert@outlook.com>
2017-10-10 10:56:56 +0200

    Finished Arrays 1 c task

--- Week 2/Day 2/1.c/Arrays/main.c
+++ Week 2/Day 2/1.c/Arrays/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char my_name_array[] = "Szücs Norbert";
+    char my_name_array[14] = "Szucs Norbert";
 
-        puts(my_name_array);
+ //       puts(my_name_array);
 
 	//TODO:
     // Print out the array character-by-character
 
+        for (int i = 0; i < 14; i++){
+ //           for (int j = 0; j < i; j++)
+            printf("%c\n", my_name_array[i]);
+        }
     return 0;
 }
