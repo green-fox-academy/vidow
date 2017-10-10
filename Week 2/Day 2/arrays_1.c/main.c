174a253190bfd0acd1a0412dc8616e922b14a94f
Vidow <szucsnorbert@outlook.com>
2017-10-10 11:12:20 +0200

    Modified task one to be in proper format

---
+++ Week 2/Day 2/arrays_1.c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    char my_name_array[14] = "Szucs Norbert";
+
+ //       puts(my_name_array);
+
+	//TODO:
+    // Print out the array character-by-character
+
+        for (int i = 0; i < 14; i++){
+
+            printf("%c\n", my_name_array[i]);
+        }
+    return 0;
+}
