190c76d8fac8e69b8efcc6e3ef710571c998b6a4
Vidow <szucsnorbert@outlook.com>
2017-10-10 11:35:03 +0200

    Added input_1.c exercise

---
+++ Week 2/Day 2/input_1.c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char buffer[255];   // Buffer variable for user input
+
+    //TODO:
+    // Get the user's name with scanf
+
+    printf("Enter name: ");
+    scanf("%s", buffer);
+    printf("Entered name is: %s", buffer);
+
+    //TODO:
+    // Print it out with printf
+
+    return 0;
+}
