054a57665bc181e0ce76d12c4cf7cceb85703ae5
Vidow <szucsnorbert@outlook.com>
2017-10-11 14:01:21 +0200

    Added advanced_4 task

--- Week 2/Day 2/advanced_4.c/main.c
+++ Week 2/Day 2/advanced_4.c/main.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+
 int main()
 {
     //write a program which loads up fully a 50-sized integer array starting from 200 in descending order
@@ -6,13 +8,48 @@ int main()
     //print out the array
 
     int my_array[50];
-    int number = 200;
+//    int number = 200;
+    int elements = 200;
+//    int temp = 0;
 
+        printf("The size of the array is: ");
 
-        for (int i = 0; i < number; i++){
+            for (int i = 0; i < 50; i++){
 
-                my_array[i] = i;
+                    my_array[i] = i;
+
+                printf("\n%d", my_array[i]);
+        }
+
+        for (int i = 200; i < 0; i--){
+
+            my_array[i] = i;
+
+            printf("\n%d", my_array[i]);
 
         }
+/*
+        for (int i = 0; i < 50; i++){
+
+            my_array[i] = i;
+
+            for (int j = 200; j > size; j--){
+
+            }
+
+       }
+
+        printf("Elements in descending order: ");
+
+        for (int i = 0; i < size; i++){
+
+            printf("\n%d", my_array[i]);
+        }
+*/
     return 0;
 }
+/*
+printf("Elements in descending order: ");
+
+            printf("\n%d", my_array[i]);
+*/
