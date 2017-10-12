1843258fa3d3c94f483dc5d4580f05226ee796ab
Vidow <szucsnorbert@outlook.com>
2017-10-12 17:19:02 +0200

    Added random generator

--- Week 2/Day 4/Guess_my_number/main.c
+++ Week 2/Day 4/Guess_my_number/main.c
@@ -1,8 +1,58 @@
+/*
+
+Exercise
+
+Write a program where the program chooses a number between 1 and 100.
+The player is then asked to enter a guess.
+If the player guesses wrong, then the program gives feedback
+and ask to enter an other guess until the guess is correct.
+
+Make the range customizable (ask for it before starting the guessing).
+You can add lives. (optional)
+Example
+
+I've the number between 1-100. You have 5 lives.
+
+20
+Too high. You have 4 lives left.
+
+10
+Too low. You have 3 lives left.
+
+15
+Congratulations. You won!
+
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+srand(time(NULL));
+
+    int r = 1 + rand() % 99;
+    int number = 0;
+
+    printf("Wanna play? I have a number between 1 and 100. Guess what is that!\n");
+
+
+    scanf("%d", &number);
+
+        if (number = r)
+
+            printf("\nCongrats, you won!");
+
+            else if (number > r)
+
+                printf("\nToo high, guess again.");
+
+            else
+
+                printf("\nToo low, guess again.");
+
+
+return 0;
 }
