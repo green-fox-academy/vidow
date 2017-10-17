aabf6d2f0c7d362a9434fc52adc87d8601ff8dd6
Vidow <szucsnorbert@outlook.com>
2017-10-17 11:19:45 +0200

    Added secnd task for type casting

---
+++ Week 3/Day 2/02.c/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	double x = 1.2, result;
+
+    //TODO: write a program wich adds up x and 1, and print out it rounded.
+
+    result = x + 1;
+
+    printf("%.f", result);
+
+    return 0;
+}
