a75cf109da8dc0ddbcd4d74cdf03ac9ae1d5a47d
Vidow <szucsnorbert@outlook.com>
2017-10-17 17:04:36 +0200

    Added transform 10 task solution

--- Week 3/Day 2/10.c/main.c
+++ Week 3/Day 2/10.c/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+//    printf("Bucky is awesome and loves tuna:\n\n");
+    char string[256] = "Bucky is awesome and loves tuna. PERIOD.";
+
+    //TODO: write a c program which, splits a string to words by space
+	//solve the problem with the proper string.h function
+
+	char *split;
+	printf("Splitting sting of '%s' into tokens:\n\n", string);
+	split = strtok(string, " ");
+
+	while (split != NULL){
+
+        printf("%s\n", split);
+        split = strtok(NULL, " ");
+	}
+
+
+    return(0);
 }
