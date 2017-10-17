a1ab92d41306f748a6eb800bb5a25912753a95c1
Vidow <szucsnorbert@outlook.com>
2017-10-17 16:54:15 +0200

    Added possible solution

--- Week 3/Day 2/08.c/main.c
+++ Week 3/Day 2/08.c/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void transform (char str[], int original_base, int new_base);
+
+//TODO: write a program, which transforms a number from a number system to another.
+//use the stdlib.h functions, and take care of error handling (see the reference)
+//the transform() function should print out the result.
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char str[20];
+    int original_base;
+    int new_base;
+
+    printf("Give me a number to transform it in an other system: ");
+    scanf("%s", str);
+    printf("Give me the original base: ");
+    scanf("%d", &original_base);
+    printf("Give me the number of the new base: ");
+    scanf("%d", &new_base);
+
+    transform(str, original_base, new_base);
+    return(0);
+}
+
+void transform (char str[], int original_base, int new_base)
+{
+    char buffer[20];
+    char *ptr;
+
+    long num = strtol(str, &ptr, original_base);
+
+    printf("%d", num);
+
+    itoa(num, buffer, new_base);
+    printf("%s\n", buffer);
 }
