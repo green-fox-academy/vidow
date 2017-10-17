06c6884c0a3bfa027b700651184a74bfa1a4c48a
Vidow <szucsnorbert@outlook.com>
2017-10-17 11:44:38 +0200

    Added binary and decimal solution

--- Week 3/Day 2/03.c/main.c
+++ Week 3/Day 2/03.c/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
+	int a = 54325;
+    char buffer[20];
+
+    strcpy(buffer, a);
+
+    itoa(a, buffer, 2);
+    printf("Binary value = %s\n", buffer);
+
+    itoa(a, buffer, 10);
+    printf("Decimal value = %s\n", buffer);
+
+    //TODO: make an integer to string conversion with itoa(), first in binary, then decimal format
+    // Store the string in buffer
+
     return 0;
 }
