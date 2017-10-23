d58ec4ba5e20d2a89950d5b9c28b4a8c83ba7e91
Vidow <szucsnorbert@outlook.com>
2017-10-23 16:57:53 +0200

    Added alternative solution to task 12.

--- Week 3/Day 2/12.c/main.c
+++ Week 3/Day 2/12.c/main.c
@@ -13,10 +13,28 @@ int main ()
 	while (p != NULL){
 	printf("%s", p);
 
-	p =strtok(NULL, to_find);
+	p = strtok(NULL, to_find);
 
 	}
 
 	//TODO: write a program, which lists all position where character 'i' is found
 	return 0;
 }
+//Alternative solution
+/*
+int main ()
+{
+	char string[55] ="This is a string for testing";
+	char *p;
+
+	//TODO: write a program, which lists all position where character 'i' is found
+
+	int k = 1;
+	p = strchr (string, 'i');
+	while (p != NULL) {
+		printf ("Character i found at position %d\n", p - string + 1);
+		p = strchr(p + 1, 'i');
+	}
+	return 0;
+}
+*/
