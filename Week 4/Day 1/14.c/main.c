2c808cfb0fa8b8bf4909286808da3cd31cfbeb8f
Vidow <szucsnorbert@outlook.com>
2017-10-24 16:45:10 +0200

    Added task 14 with two solutions.

--- Week 4/Day 1/14.c/main.c
+++ Week 4/Day 1/14.c/main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
-void string_copy(char *dest, char *source);
+void string_copy(char *dest, char *source)
+{
+    int temp = 0;
+    int i = 0;
+
+    for (i = 0; i < strlen(source); i++) {
+
+        dest[i] = source[i];
+    }
+
+    dest[i] = '\0';
+
+    printf("%s\n", dest);
+}
 
 int main()
 {
     char my_string[] = "to be copied";
     char dest[50];
+
+    string_copy(dest, my_string);
 
    //TODO: write a function, which copies a string to the 'dest' array, then print out in string format.
 
