7c0089b1c3eb39bc68d39f7530da589bc96fe7ea
Vidow <szucsnorbert@outlook.com>
2017-10-24 12:52:49 +0200

    Added task 1 solution.

--- Week 4/Day 1/01.c/main.c
+++ Week 4/Day 1/01.c/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int number = 1234;
+    //TODO:
+    // print the memory address of the variable
+
+    int *a = 0;
+    a = &number;
+    //Printing memory address of the variable:
+    printf("The memory address of 'number' is %p.\n", a);
+    //or:
+    printf("The memory address of 'number' is %p.\n", &number);
+    //Printing the value of the variable:
+    printf("The value of number is %d.\n", *a);
+
     return 0;
 }
