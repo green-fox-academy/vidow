557e1b204c07b929f723cef0cba60154d65e1d2d
Vidow <szucsnorbert@outlook.com>
2017-10-24 17:38:09 +0200

    Added 1 for loop for task 15, need 2 more.

--- Week 4/Day 1/15.c/main.c
+++ Week 4/Day 1/15.c/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-void reverse_name(char *name, char *reversed_name);
+void reverse_name(char *name, char *reversed_name)
+{
+    int i = 0;
+    int j = 0;
+    int temp[];
+
+    for (i = 0; name[i] != ' '; i++) {
+
+        temp[i] = name[i];
+    }
+
+    for (j = name[i]; name[j] != '\0'; j++) {
+
+        reversed_name[i + j] = name[j];
+    }
+
+    printf("%s", reversed_name);
+}
 
 int main(void)
 {
 	char name[] = "Gipsz Jakab";
 
     char reversed[50];
+
+    reverse_name(name, reversed);
 
     //write a function which reverse a name's last name and first name. Use a void function. Put your 'return' value in the second parameter.
 
