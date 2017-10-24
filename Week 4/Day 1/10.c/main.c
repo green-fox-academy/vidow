7497f53bf70e81988eca16fae328b0ab40d93ba9
Vidow <szucsnorbert@outlook.com>
2017-10-24 15:58:03 +0200

    Added task 10 with solution.

---
+++ Week 4/Day 1/10.c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main()
+{
+	int x, y;
+	x = 5;
+	y = 8;
+	printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+
+	swap(&x, &y);
+
+    printf("After swapping\nx = %d\ny = %d\n", x, y);
+
+	//TODO: write a function which swaps the values of x and y. Then print out the values of x and y.
+
+	return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
