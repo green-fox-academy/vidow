b2fc8853af76b78c8eaea88306a234f291ba2a8c
Vidow <szucsnorbert@outlook.com>
2017-10-24 14:04:57 +0200

    Added solution which is bugged.

--- Week 4/Day 1/06.c/main.c
+++ Week 4/Day 1/06.c/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int high_number = 6655;
+    int low_number = 2;
+
+    int* hight_number_pointer = &high_number;
+    int* other_high_number_pointer;
+
+
+    //TODO:
+    // The "other_high_number_pointer" should point to the same memory address
+    // without using the "&" operator.
+
+    other_high_number_pointer = *hight_number_pointer;
+
+    printf("Memory address of hight_number_pointer is %p\nMemory address of other_high_number_pointer is %p.\n", &hight_number_pointer, &other_high_number_pointer);
+
     return 0;
 }
