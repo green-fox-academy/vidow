64e7ecc86cf8110834b578731b7b6a8c6a10798f
Vidow <szucsnorbert@outlook.com>
2017-10-24 21:28:30 +0200

    Added alternative solution to task 15.

--- Week 4/Day 1/15.c/main.c
+++ Week 4/Day 1/15.c/main.c
@@ -3,31 +3,39 @@
 
 void reverse_name(char *name, char *reversed_name)
 {
+
+    int space_temp = 0;
     int i = 0;
-    int j = 0;
-    int temp[];
+    strcpy(reversed_name, name);
 
-    for (i = 0; name[i] != ' '; i++) {
-
-        temp[i] = name[i];
+    for (i = 0; i < strlen(name); i++) {
+        if (name[i] == ' ') {
+            space_temp = i;
+            break;
+        }
+    }
+    for (i = 0; i < strlen(name) - space_temp - 1; i++) {
+        reversed_name[i] = name[space_temp + 1 + i];
     }
 
-    for (j = name[i]; name[j] != '\0'; j++) {
+    reversed_name[strlen(name) - space_temp - 1] = ' ';
 
-        reversed_name[i + j] = name[j];
+    for (i = 0; i < space_temp; i++) {
+        reversed_name[strlen(name) - space_temp + i] = name[i];
     }
 
-    printf("%s", reversed_name);
+    reversed_name[strlen(reversed_name) + 1] = '\0';
+
+    printf("%s\n", reversed_name);
 }
 
 int main(void)
 {
-	char name[] = "Gipsz Jakab";
+	char name[] = "Jean-Claud VanDame";
 
     char reversed[50];
 
     reverse_name(name, reversed);
 
     //write a function which reverse a name's last name and first name. Use a void function. Put your 'return' value in the second parameter.
-
 }
