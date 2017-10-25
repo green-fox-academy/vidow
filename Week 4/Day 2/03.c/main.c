52b6d2766b6ee0403760df2c3ec434649533540d
Vidow <szucsnorbert@outlook.com>
2017-10-25 17:30:51 +0200

    Added task 03 solution, finaly.

--- Week 4/Day 2/03.c/main.c
+++ Week 4/Day 2/03.c/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct {
     int x;
     int y;
 } point_t;
+
+
+point_t result(int x, int y);
+float distance(point_t p1, point_t p2);
 
 // TODO:
 // Create a function the constructs a point
@@ -13,7 +18,33 @@ typedef struct {
 // TODO:
 // Create a function that takes 2 Points as a pointer and returns the distance between them
 
+
+
 int main()
 {
+
+    point_t coord1 = result(10, 10);
+    point_t coord2 = result(9, 9);
+
+    float r = distance(coord1, coord2);
+
+    printf("%.2f", r);
+
     return 0;
+}
+
+point_t result(int x, int y)
+{
+
+    point_t p;
+    p.x = x;
+    p.y = y;
+
+    return p;
+
+}
+
+float distance(point_t p1, point_t p2)
+{
+    return sqrt(pow((p2.x - p1.x),2) + ((p2.y - p1.y),2));
 }
