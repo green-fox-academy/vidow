52f3df173851abd4cb42872678ac399e77fb1947
Vidow <szucsnorbert@outlook.com>
2017-10-26 08:54:29 +0200

    Added main menu to ToDoApp.

--- Week 4/Day 3/ToDoApp/main.c
+++ Week 4/Day 3/ToDoApp/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
+}
+
+void main_menu()
+{
+    printf( "           Todo application\n"
+            "========================================\n"
+            "Commands:\n"
+            "-a   Adds a new task\n"
+            "-wr  Write current todos to file\n"
+            "-rd  Read todos from a file\n"
+            "-l   Lists all the tasks\n"
+            "-e   Empty the list\n"
+            "-rm  Removes a task\n"
+            "-c   Completes a task\n"
+            "-p   Add priority to a task\n"
+            "-lp  Lists all the tasks by priority\n");
 }
