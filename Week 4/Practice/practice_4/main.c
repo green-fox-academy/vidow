77eb1902609f90fac21d4878ef39631e75aea156
Vidow <szucsnorbert@outlook.com>
2017-10-28 18:45:33 +0200

    Added practice 4 with solution.

---
+++ Week 4/Practice/practice_4/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//fill abc string from a to z, print out
+
+int main()
+{
+    char alphabet[25] = {"abcdefghijklmnopqrstuwxyz"};
+
+    for (int i = 0; i <= sizeof(alphabet) / sizeof(alphabet[0]; i++) {
+
+        printf("%c\n", alphabet[i]);
+    }
+
+    return 0;
+}
