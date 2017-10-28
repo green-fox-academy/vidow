7bfbab1200b2e80c215640ef7e916a8a1c05c499
Vidow <szucsnorbert@outlook.com>
2017-10-28 18:46:09 +0200

    Added practice 8.

---
+++ Week 4/Practice/practice_8/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+POINTER FOR STRINGS
+Every task should be in different function, and use pointers
+1, fill your string with your name, sum the length of your array,
+return with the length, print it out in the main()
+2, count the 'a'-s (or something else) in your string, return with it,
+and print out in the main() function
+3, Change the capital letter to lowercase and the lowercase into capital letter
+- print out in the main() function*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
