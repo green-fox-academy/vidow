cea08981d9fe63d44c8c3c11ff72e17c3a533a54
Vidow <szucsnorbert@outlook.com>
2017-10-30 15:45:54 +0100

    Added array_02 task with solution and comments.

--- Week 5/Day 1/array_02.c/main.c
+++ Week 5/Day 1/array_02.c/main.c
@@ -10,23 +10,24 @@
 
 int main()
 {
+    //Loaded array with 12 random numbers.
     int numbers[12] = {10, 5, 8, 18, 12, 19, 15, 33, 44, 17, 23, 56};
-    int temp = 0;
+    int temp = 0; //Temp variable to store data later.
     int lenght = sizeof(numbers) / sizeof(0);
-
+    //For loop to get the numbers in the numbers array.
     for (int i = 0; i < lenght; i++) {
-
+        //Second for loop to recheck the numbers.
         for (int j = 0; j < lenght - 1; j++) {
-
+            //Condition to start to check the value of numbers
             if (numbers[j] > numbers[j + 1]) {
-
+                //Procedure to check the number in ascending order. For descending use "<" instead of ">" in if condition.
                 temp = numbers[j];
                 numbers[j] = numbers[j + 1];
                 numbers[j + 1] = temp;
             }
         }
     }
-
+    //Another loop to print out the numbers in ascending order.
     for (int i = 0; i < lenght; i++) {
 
         printf("%d\n", numbers[i]);
