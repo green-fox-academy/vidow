218cfe7335bbe725fea0d96443c62ac8683db498
Vidow <szucsnorbert@outlook.com>
2017-10-31 13:36:24 +0100

    Added array_function_02 task.

---
+++ Week 5/Day 1/array_function_02.c/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create a function that takes two arrays of characters and a number.
+ * It should replace the contents of the first array from the given index
+ * with the contents of the second array.
+ * The function should return a number which tells whether it was successful:
+ *    * If all is right it should return 0.
+ *    * If the index is out of bounds it should return -1.
+ *    * If the inserted content would be longer than the receiving array it should return 1 and leave the array as it is.
+ * IMPORTANT: Think about what kind of arguments You need to make this happen and craft the function accordingly.
+ *
+ * Write code in the main function to test all scenarios and write out the result You need to check whether this works.
+ */
+
+int main()
+{
+
+    return 0;
+}
