a2fc245a3de71f7461334934bc34da7cd19edb85
Vidow <szucsnorbert@outlook.com>
2017-11-02 11:23:50 +0100

    Added struct 00 task with comments.

--- Week 5/Day 1/structures_functions_00.c/main.c
+++ Week 5/Day 1/structures_functions_00.c/main.c
@@ -23,7 +23,7 @@
 
 
 
-
+//Seting struchture and define it as "library".
 typedef struct Books{
     char title[50];
     char author[50];
@@ -40,52 +40,58 @@ typedef struct Books{
 
 int main()
 {
-
+    //Printing out book C_language with the below title, author and date.
     library C_language = create_new_book("The C Programming Language", "W. Kernighan and Dennis Ritchie", 2000);
     book_printer(C_language);
-
+    //Changing only the title of the previously created book.
     set_title(&C_language, "18 happy student with A+ exam result");
     book_printer(C_language);
-
+    //Changing only the author of the previously created book.
     set_author(&C_language, "Norbert Szucs");
     book_printer(C_language);
-
+    //Changing only the issue date of the previously created book.
     set_publications(&C_language, 2017);
     book_printer(C_language);
 
 
     return 0;
 }
-
+//Function to create a new book. We can use "library" variable since it has been created during the struct definition.
 library create_new_book(char *title, char *author, int publications)
 {
+    //To create "new_book" with "library" variable.
     library new_book;
 
-    strcpy(new_book.title, title);
-    strcpy(new_book.author, author);
-    new_book.publications = publications;
+    strcpy(new_book.title, title); //To copy the title. It has to be done with stringcopy because it is an array.
+    strcpy(new_book.author, author); //To copy the author. It has to be done with stringcopy because it is an array.
+    new_book.publications = publications; //To copy the issue date.
 
     return new_book;
 }
 
+//Function to set a new title.
 void set_title(library *book, char *new_title)
 {
     strcpy(book->title, new_title);
 
 }
 
+//Function to set a new author.
 void set_author(library *book, char *new_author)
 {
     strcpy(book->author, new_author);
 
 }
 
+//Function to set new issue date.
 int set_publications(library *book, int *new_publications)
 {
+    //-> means the pointer. It could be ((*book).publications = new_publications also.
     book->publications = new_publications;
 
 }
 
+//Function to print title, author and publication date.
 void book_printer(library book)
 {
 
