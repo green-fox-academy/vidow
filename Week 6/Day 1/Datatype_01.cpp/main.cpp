1c8f8369ea8f4b517daddb28b6057a10f987c36b
Vidow <szucsnorbert@outlook.com>
2017-11-13 13:18:34 +0100

    Added first task solution for second exercise for C++, Datatypes.

--- Week 6/Day 1/Datatype_01.cpp/main.cpp
+++ Week 6/Day 1/Datatype_01.cpp/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int avg_coding = ((7 * 17) - (17 * 2)) * 6;
+
+    cout << "A Green Fox attendee codes " << avg_coding << " hours during a semester which is 85 working days." << endl;
+
     return 0;
 }
