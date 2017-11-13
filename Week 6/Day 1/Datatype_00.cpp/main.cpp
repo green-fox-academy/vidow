a74e2901d425ae73c50bc63131883de09d5f6aa8
Vidow <szucsnorbert@outlook.com>
2017-11-13 13:04:36 +0100

    Added first task for C++, Datatypes.

---
+++ Week 6/Day 1/Datatype_00.cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Write a program that prints a few details to the terminal window about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Do you like coffee? (as a boolen variable)
+//
+//  Example output:
+//  John Doe
+//  31
+//  Likes coffee: true
+
+int main()
+{
+
+
+
+    return 0;
+}
