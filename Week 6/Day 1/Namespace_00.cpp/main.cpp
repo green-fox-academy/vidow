965eb1f3bc164edbe23425b711e1d14657fa77db
Vidow <szucsnorbert@outlook.com>
2017-11-13 14:06:27 +0100

    Added first Namespace task solution for C++.

--- Week 6/Day 1/Namespace_00.cpp/main.cpp
+++ Week 6/Day 1/Namespace_00.cpp/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-using namespace std;
+// Make it work, without using namespace
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std :: cout << "Hello world!" << std :: endl;
     return 0;
 }
