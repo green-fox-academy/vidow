cae135ef3ac30e3c5fab23d23785a45347ce05fe
Vidow <szucsnorbert@outlook.com>
2017-11-13 15:13:12 +0100

    Added Namespace exercise four solution for C++.

--- Week 6/Day 1/Namespace_03.cpp/main.cpp
+++ Week 6/Day 1/Namespace_03.cpp/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+using namespace std;
+
 
 // Use your costume namespace from the last exercise
 // Create a function, which takes the age as a parameter
 // and returns the year when you were born
 
+namespace Person{
+
+    string MyName = "Norbert Szucs";
+    int MyAge = 31;
+    int MyFavNum = 27;
+}
+
+int BirthYear(int MyAge);
+
 int main()
 {
+    cout << BirthYear(Person::MyAge) << endl;
 
     return 0;
+}
+
+int BirthYear(int MyAge)
+{
+
+    return 2017 - MyAge;
+
 }
