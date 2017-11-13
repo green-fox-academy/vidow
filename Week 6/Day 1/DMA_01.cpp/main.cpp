20ff2c99cfff54bbb495a6646cc9f13f356d1d70
Vidow <szucsnorbert@outlook.com>
2017-11-13 17:19:08 +0100

    Added DMA task 2 for C++.

---
+++ Week 6/Day 1/DMA_01.cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// Please allocate a 5 long array and fill it with numbers from 0 to 4, then print the whole array
+// Please delete the array before the program exits
+
+int main() {
+  int* pointer = NULL;
+
+
+  return 0;
+}
