9fe2fe13a34881c5a717b4686f092ce153745e00
Vidow <szucsnorbert@outlook.com>
2017-11-13 12:59:44 +0100

    Added second task for C++, Console io.

---
+++ Week 6/Day 1/01.cpp/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+
+int main()
+{
+    string classmate_1 = "Esther";
+    string classmate_2 = "Mary";
+    string classmate_3 = "Joe";
+
+    cout << "Hello " << classmate_1 << "!" << endl;
+    cout << "Hello " << classmate_2 << "!" << endl;
+    cout << "Hello " << classmate_3 << "!" << endl;
+
+    return 0;
+}
