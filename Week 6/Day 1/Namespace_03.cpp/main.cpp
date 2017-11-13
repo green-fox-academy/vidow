34014af211e7035df04dc04365b3d1173e7884c0
Vidow <szucsnorbert@outlook.com>
2017-11-13 14:57:18 +0100

    Added Namespace exercise 4 for C++.

---
+++ Week 6/Day 1/Namespace_03.cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Use your costume namespace from the last exercise
+// Create a function, which takes the age as a parameter
+// and returns the year when you were born
+
+int main()
+{
+
+    return 0;
+}
