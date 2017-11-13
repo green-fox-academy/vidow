17a72fe139797f3226a2a12abef689a816da63df
Vidow <szucsnorbert@outlook.com>
2017-11-13 16:50:20 +0100

    Added Vector task 4 with solution in C++.

--- Week 6/Day 1/Vector_03.cpp/main.cpp
+++ Week 6/Day 1/Vector_03.cpp/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,6 +10,27 @@ int main() {
 	//fill this vector with random numbers from 0 to 10
 	//print the items of the vector
   //remove the even numbers, then print the items again
+
+    int size = 20;
+	vector<int> MyVector(size);
+
+	for (int i = 0; i < size; i++) {
+        MyVector.at(i) = rand() % 10;
+        cout << MyVector[i] << " ";
+	}
+
+	cout << endl;
+
+    for (int i = 0; i < size; i++) {
+        if (MyVector.at(i) % 2 == 0) {
+            MyVector.erase(MyVector.begin() + i);
+            size--;
+            i--;
+        }
+    }
+	for (int i = 0; i < size; i++) {
+        cout << MyVector[i] << " ";
+	}
 
   return 0;
 }
