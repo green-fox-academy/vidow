8694b439187b9efaf068a77d4c4e02c64a29484b
Vidow <szucsnorbert@outlook.com>
2017-11-13 11:49:18 +0100

    Added first task for C++, Console io.

---
+++ Week 6/Day 1/00.cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string me = "Norbi";
+
+    cout << "Hello " << me <<endl;
+    return 0;
+}
