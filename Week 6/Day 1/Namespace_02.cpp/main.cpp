b38bb72c435d270b87a2d7ed57e6173e39468efa
Vidow <szucsnorbert@outlook.com>
2017-11-13 14:09:12 +0100

    Added third Namespace task for C33.

---
+++ Week 6/Day 1/Namespace_02.cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Create your own namespace, called Person
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+// write them out in main()
+
+int main()
+{
+
+    return 0;
+}
