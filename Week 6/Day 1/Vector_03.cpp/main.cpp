312bfb75ddef8986853ee8af6df10d57d9a9e0bf
Vidow <szucsnorbert@outlook.com>
2017-11-13 16:21:42 +0100

    Added Vector exercise 4 with solution for C++.

---
+++ Week 6/Day 1/Vector_03.cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//create a vector of integers with the size of 20
+	//fill this vector with random numbers from 0 to 10
+	//print the items of the vector
+  //remove the even numbers, then print the items again
+
+  return 0;
+}
