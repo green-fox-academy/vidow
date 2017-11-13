e93ec2052c6711462add1c14d38e56f086d1c77c
Vidow <szucsnorbert@outlook.com>
2017-11-13 17:16:18 +0100

    Added solution for DMA task 1 for C++.

--- Week 6/Day 1/DMA_00.cpp/main.cpp
+++ Week 6/Day 1/DMA_00.cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,8 +8,41 @@ using namespace std;
 // allocate memory dynamically for these variables
 // create a sentence using these variables
 // free the allocated memory
+/*
+namespace Variables{
 
+    string name = "Norbert Szucs";
+    int age = 31;
+    int slice_of_pizza_you_can_eat = 1000;
+}
+*/
 int main() {
+/*
+    string name = "Norbert Szucs";
+    int age = 31;
+    int slice_of_pizza_you_can_eat = 1000;
+*/
+    int *age;
+    string *name;
+    int *slice_of_pizza_you_can_eat;
+
+    age = new int;
+    *age = 31;
+
+    name = new string;
+    *name = "Norbert Szucs";
+
+    slice_of_pizza_you_can_eat = new int;
+    *slice_of_pizza_you_can_eat = 1000;
+/*
+    cout << Variables::name << " who is " << Variables::age << " years old can eat " << Variables::slice_of_pizza_you_can_eat << " slices of pizza.";
+*/
+    cout << *name << " who is " << *age << " years old can eat " << *slice_of_pizza_you_can_eat << " slices of pizza.";
+
+
+    delete age;
+    delete name;
+    delete slice_of_pizza_you_can_eat;
 
 
   return 0;
