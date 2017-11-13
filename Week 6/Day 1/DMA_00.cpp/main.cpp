cfb4e7d4d880190a9538b6650f2b4a3a083ff976
Vidow <szucsnorbert@outlook.com>
2017-11-13 16:51:52 +0100

    Added DMA task 1 for C++.

---
+++ Week 6/Day 1/DMA_00.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// store your personal data in Variables
+// string name, int age, int slice_of_pizza_you_can_eat
+// allocate memory dynamically for these variables
+// create a sentence using these variables
+// free the allocated memory
+
+int main() {
+
+
+  return 0;
+}
