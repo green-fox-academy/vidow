54269a584013718d92137c1f25a38235f0eac634
Vidow <szucsnorbert@outlook.com>
2017-11-14 11:22:59 +0100

    Added second exercise with solution for OOP, C++.

--- Week 6/Day 2/oop_02.cpp/main.cpp
+++ Week 6/Day 2/oop_02.cpp/main.cpp
@@ -15,11 +15,17 @@ class Student {
       this->age = age;
       this->name = name;
     }
+    void printer(){
+        cout << name << " is " << age << " years old." << endl;
+    }
 };
 
 int main() {
   // Create a new instace of the Student class and set it's name to "John" and
   // it's age to 21 with it's constructor
+
+    Student so("John", 21);
+    so.printer();
 
   return 0;
 }
