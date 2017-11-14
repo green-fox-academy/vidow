95b4052a2321379d69348c7096634c6d7ed0c16a
Vidow <szucsnorbert@outlook.com>
2017-11-14 11:14:31 +0100

    Added Levi example for OOP, C++.

---
+++ Week 6/Day 2/Levi_example/main.cpp
@@ -0,0 +1,42 @@
+include <iostream>
+
+using namespace std;
+
+class BankAcc{
+private:
+    int pin_code;
+    float money;
+
+public:
+    BankAcc(int pin_code, float money){
+        this->pin_code = pin_code;
+        this->money = money;
+    }
+    ~BankAcc(){
+        cout << "sms with acc data have been sent" << endl;
+    }
+    void print_data(){
+        cout << "My pin is " << pin_code << " and I have " << money << " forints" << endl;
+    }
+    void set_money(int pin_code, float money){
+        if (this->pin_code == pin_code){
+            this->money = money;
+        } else {
+            cout << "nice try hacker!" << endl;
+        }
+    }
+
+};
+
+int main()
+{
+    BankAcc ba(1234, 33.33);
+
+    ba.print_data();
+
+    ba.set_money(134, 1200);
+
+    ba.print_data();
+
+    return 0;
+}
