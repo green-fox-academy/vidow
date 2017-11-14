11993cd73449e3726b4380957503059eab0a7741
Vidow <szucsnorbert@outlook.com>
2017-11-14 16:27:58 +0100

    Separated second exercise to another cpp and h file.

--- Week 6/Day 2/oop_02.cpp/main.cpp
+++ Week 6/Day 2/oop_02.cpp/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
 #include <string>
+#include "oop_02.h"
 
 using namespace std;
-
-class Student {
-  public:
-        Student(unsigned int age, string name){
-        this->age = age;
-        this->name = name;
-    }
-    string name;
-    unsigned int age;
-    Student(string name, unsigned int age) {
-      this->age = age;
-      this->name = name;
-    }
-    void printer(){
-        cout << name << " is " << age << " years old." << endl;
-    }
-};
 
 int main() {
   // Create a new instace of the Student class and set it's name to "John" and
