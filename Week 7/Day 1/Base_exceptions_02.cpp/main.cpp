2ffc8e280c1c1cb2f8d57f38e9140078f412712c
Vidow <szucsnorbert@outlook.com>
2017-11-20 13:09:10 +0100

    Added Base Exceptions task two solution, C++.

--- Week 7/Day 1/Base_exceptions_02.cpp/main.cpp
+++ Week 7/Day 1/Base_exceptions_02.cpp/main.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <windows.h>
+#include <stdexcept>
+
 using namespace std;
 
 // Write a try - catch block.
 // Throw an char in the try block
 // Catch it in the catch block and write it out.
 
-
+int _div(int _a, int _b){
+    if (_b == 0){
+        throw "x";
+    }
+    return _a / _b;
+}
 
 int main() {
 
     int a = 0;
+    cin >> a;
     int b = 0;
+    cin >> b;
+
+    try {
+        cout << _div(a, b) << endl;
+    }
+    catch(const char *msg){
+            cout << msg << endl;
+    }
 
 
 	return 0;
