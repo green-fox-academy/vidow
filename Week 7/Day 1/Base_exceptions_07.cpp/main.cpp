f5107e94f33ebe46fa5412de907075ac7a61917e
Vidow <szucsnorbert@outlook.com>
2017-11-20 14:19:53 +0100

    Added Base Exception task 7 with another solution, C++.

--- Week 7/Day 1/Base_exceptions_07.cpp/main.cpp
+++ Week 7/Day 1/Base_exceptions_07.cpp/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 //Try to run the following code!
 
-void WrongLength(int a[]){
+void WrongLength(int *a[]){
     if((sizeof(a) / sizeof(a[0])) < 6){
         throw 1;
     }
@@ -15,8 +15,9 @@ int main () {
     try{
     int int_array[5] = {1, 2, 3, 4, 5};
 
+    cout << int_array[200] << endl;
+
     if (sizeof(int_array) / sizeof(int_array[0]) < 6){
-        cout << int_array[200] << endl;
         throw runtime_error("Array is smaller than the requested member of that array.");
     }else if (sizeof(int_array) / sizeof(int_array[0]) < sizeof(int_array)){
         throw runtime_error("Array size is just perfetto.");
