c8bd9b551cbf5054ee4d5d2ee73c8f969d333cc2
Vidow <szucsnorbert@outlook.com>
2017-11-20 12:46:34 +0100

    Added Base Exceptions task two, C++.

---
+++ Week 7/Day 1/Base_exceptions_02.cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Write a try - catch block.
+// Throw an char in the try block
+// Catch it in the catch block and write it out.
+
+
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+
+
+	return 0;
+}
