40d246ebe4f57357ac4b6d3963fec33c5cfa99ac
Vidow <szucsnorbert@outlook.com>
2017-11-21 15:53:30 +0100

    Added Overloading Task 04, C++.

---
+++ Week 7/Day 2/cpp_overloading_04.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Create a Class named Integer. It should have one int member variable.
+// Create add() methods to it. They should take Integer& parameters.
+// Make variations of add() method 1, 2, 3, 4 and 5 arguments respectively.
+// All methods should add the value of the parameters to the member variable of this.
+//
+// Try them out. Write code that demonstrates how they work.
+
+int main() {
+
+
+	return 0;
+}
