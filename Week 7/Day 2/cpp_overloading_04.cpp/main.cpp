b81e225253dfb436e7828d5b747d65fa2ff72a20
Vidow <szucsnorbert@outlook.com>
2017-11-21 16:23:42 +0100

    Added Overloading Task 04 solution, C++.

--- Week 7/Day 2/cpp_overloading_04.cpp/main.cpp
+++ Week 7/Day 2/cpp_overloading_04.cpp/main.cpp
@@ -8,8 +8,44 @@ using namespace std;
 //
 // Try them out. Write code that demonstrates how they work.
 
+class Integer{
+private:
+    int mNum;
+
+public:
+    Integer(int a){
+        mNum = a;
+    };
+    int get_mNum(){
+        return mNum;
+    };
+    int add(Integer& _a){
+        return mNum + _a.get_mNum();
+    };
+    int add(Integer& _a, Integer& _b){
+        return mNum + _a.get_mNum() + _b.get_mNum();
+    };
+    int add(Integer& _a, Integer& _b, Integer& _c){
+        return mNum + _a.get_mNum() + _b.get_mNum() + _c.get_mNum();
+    };
+    int add(Integer& _a, Integer& _b, Integer& _c, Integer& _d){
+        return mNum + _a.get_mNum() + _b.get_mNum() + _c.get_mNum() + _d.get_mNum();
+    };
+    int add(Integer& _a, Integer& _b, Integer& _c, Integer& _d, Integer& _e){
+        return mNum + _a.get_mNum() + _b.get_mNum() + _c.get_mNum() + _d.get_mNum() + _e.get_mNum();
+    };
+};
+
 int main() {
 
+    Integer io(2);
+
+    cout << io.get_mNum() << endl;
+    cout << io.add(io) << endl;
+    cout << io.add(io, io) << endl;
+    cout << io.add(io, io, io) << endl;
+    cout << io.add(io, io, io, io) << endl;
+    cout << io.add(io, io, io, io, io) << endl;
 
 	return 0;
 }
